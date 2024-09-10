963d6fda1823ea3863391908aaa22532e715a7f9
Sophia <sdowns1@outlook.com>
2024-09-10 10:10:09 -0400

    remove directory

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'blog' / 'templates'
+            
         ],
         'APP_DIRS': True,
         'OPTIONS': {
